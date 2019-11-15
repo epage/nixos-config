e5cbad206c413c7f8acddb52624ca68f52ec96af
Ed Page <epage@duosecurity.com>
2019-11-15 12:58:31 -0700

    fix: Another approach

--- profiles/default.nix
+++ profiles/default.nix
@@ -5,7 +5,7 @@
   # List packages installed in system profile. To search by name, run:
   # nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    <home-manager/install>
+    home-manager.install
 
     # Version control / archiving
     git gitAndTools.hub
